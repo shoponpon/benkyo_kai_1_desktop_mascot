a7fee2ed49b66d012cb64d7f188cf56efe9e9cd4
shoponpon <shoponprog@gmail.com>
2017-07-20 17:28:32 +0900

    Hide devtools when launch app

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function createWindow() {
         slashes: true
     }));
     
-    mainWindow.openDevTools({ mode: "detach" });
+//    mainWindow.openDevTools({ mode: "detach" });
 
     mainWindow.on('closed', () => {
         win = null
